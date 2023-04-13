5afd8c53ceb90010b11c2feda9686a3c4842e5e5
Melody Liu <33408946+melocule@users.noreply.github.com>
2023-04-13 19:29:19 -0400

    Nvm remove animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,58 +17,58 @@ export default function Home() {
   return (
     <div>
       {/* Map */}
-      <div onMouseDown={() => setShowDrawer(false)}><Map clinics={clinics} /></div>
+      <Map clinics={clinics} />
       {/* Drawer */}
-        <motion.div className="min-h-screen">
-          <div className={`absolute bg-white top-0 left-0 h-screen w-80 p-6 max-w-[40vw] overflow-scroll 
-          flex flex-col`}>
-            {/* Back Button */}
-            <button>
-              <ArrowLeftIcon
-                className={`h-8 self-start ${
-                  currentPage == "search" && "invisible"
-                }`}
-                onClick={() => setCurrentPage("search")}
-              />
-            </button>
+      <div className="min-h-screen">
+        <div className={`absolute bg-white top-0 left-0 h-screen w-80 p-6 max-w-[40vw] overflow-scroll 
+        flex flex-col`}>
+          {/* Back Button */}
+          <button>
+            <ArrowLeftIcon
+              className={`h-8 self-start ${
+                currentPage == "search" && "invisible"
+              }`}
+              onClick={() => setCurrentPage("search")}
+            />
+          </button>
 
-            {/* Tabs */}
-            <div className="tabs self-center py-8">
-              <a
-                className={`tab tab-bordered ${
-                  currentPage === "search" && "tab-active"
-                }`}
-                onClick={() => setCurrentPage("search")}
-              >
-                Search
-              </a>
-              <a
-                className={`tab tab-bordered ${
-                  currentPage === "result" && "tab-active"
-                }`}
-                onClick={() => setCurrentPage("result")}
-              >
-                Results
-              </a>
-            </div>
-
-            {/* Drawer Content */}
-            <div>
-              {currentPage == "search" && (
-                <Search
-                  onSearch={(searchParams) => {
-                    console.log("Search triggered");
-                    console.dir(searchParams);
-                    searchCriteria = searchParams;
-                    setClinics(getClinics(searchCriteria));
-                    setCurrentPage("result");
-                  }}
-                />
-              )}
-              {currentPage == "result" && <Results clinics={clinics} />}
-            </div>
+          {/* Tabs */}
+          <div className="tabs self-center py-8">
+            <a
+              className={`tab tab-bordered ${
+                currentPage === "search" && "tab-active"
+              }`}
+              onClick={() => setCurrentPage("search")}
+            >
+              Search
+            </a>
+            <a
+              className={`tab tab-bordered ${
+                currentPage === "result" && "tab-active"
+              }`}
+              onClick={() => setCurrentPage("result")}
+            >
+              Results
+            </a>
           </div>
-        </motion.div>
+
+          {/* Drawer Content */}
+          <div>
+            {currentPage == "search" && (
+              <Search
+                onSearch={(searchParams) => {
+                  console.log("Search triggered");
+                  console.dir(searchParams);
+                  searchCriteria = searchParams;
+                  setClinics(getClinics(searchCriteria));
+                  setCurrentPage("result");
+                }}
+              />
+            )}
+            {currentPage == "result" && <Results clinics={clinics} />}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
