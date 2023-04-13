331ce70d29224b14a8d098fc7020d15cf59b446b
Melody Liu <33408946+melocule@users.noreply.github.com>
2023-04-13 19:23:31 -0400

    Add opacity animation with disappearing drawer

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,61 +6,70 @@ import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import Map from "./Map.js";
 import { getClinics } from "../utils/get-clinics.js";
 import Results from "./Results.js";
+import { motion } from "framer-motion"
 
 export default function Home() {
   const [currentPage, setCurrentPage] = useState("search");
   const [clinics, setClinics] = useState([]);
+  const [showDrawer, setShowDrawer] = useState(true);
 
   let searchCriteria;
 
   return (
-    <div className="min-h-screen">
-      <Map clinics={clinics} />
-      <div className="absolute top-0 left-0 h-screen p-6 w-80 p-6max-w-[40vw] bg-white flex flex-col drop-shadow-xl overflow-scroll">
-        {/* Back Button */}
-        <button>
-          <ArrowLeftIcon
-            className={`h-8 self-start ${
-              currentPage == "search" && "invisible"
-            }`}
-            onClick={() => setCurrentPage("search")}
-          />
-        </button>
+    <div>
+      {/* Map */}
+      <div onMouseDown={() => setShowDrawer(false)} onMouseUp={() => setShowDrawer(true)}><Map clinics={clinics} /></div>
+      {/* Drawer */}
+        <motion.div className="min-h-screen" animate={showDrawer ? {opacity: 1} : {opacity: 0}}>
+          <div className={`absolute bg-white top-0 left-0 h-screen w-80 p-6 max-w-[40vw] overflow-scroll 
+          flex flex-col`}>
+            {/* Back Button */}
+            <button>
+              <ArrowLeftIcon
+                className={`h-8 self-start ${
+                  currentPage == "search" && "invisible"
+                }`}
+                onClick={() => setCurrentPage("search")}
+              />
+            </button>
 
-        {/* Tabs */}
-        <div className="tabs self-center py-8">
-          <a
-            className={`tab tab-bordered ${
-              currentPage === "search" && "tab-active"
-            }`}
-            onClick={() => setCurrentPage("search")}
-          >
-            Search
-          </a>
-          <a
-            className={`tab tab-bordered ${
-              currentPage === "result" && "tab-active"
-            }`}
-            onClick={() => setCurrentPage("result")}
-          >
-            Results
-          </a>
-        </div>
+            {/* Tabs */}
+            <div className="tabs self-center py-8">
+              <a
+                className={`tab tab-bordered ${
+                  currentPage === "search" && "tab-active"
+                }`}
+                onClick={() => setCurrentPage("search")}
+              >
+                Search
+              </a>
+              <a
+                className={`tab tab-bordered ${
+                  currentPage === "result" && "tab-active"
+                }`}
+                onClick={() => setCurrentPage("result")}
+              >
+                Results
+              </a>
+            </div>
 
-        {/* Drawer Content */}
-        {currentPage == "search" && (
-          <Search
-            onSearch={(searchParams) => {
-              console.log("Search triggered");
-              console.dir(searchParams);
-              searchCriteria = searchParams;
-              setClinics(getClinics(searchCriteria));
-              setCurrentPage("result");
-            }}
-          />
-        )}
-        {currentPage == "result" && <Results clinics={clinics} />}
-      </div>
+            {/* Drawer Content */}
+            <div>
+              {currentPage == "search" && (
+                <Search
+                  onSearch={(searchParams) => {
+                    console.log("Search triggered");
+                    console.dir(searchParams);
+                    searchCriteria = searchParams;
+                    setClinics(getClinics(searchCriteria));
+                    setCurrentPage("result");
+                  }}
+                />
+              )}
+              {currentPage == "result" && <Results clinics={clinics} />}
+            </div>
+          </div>
+        </motion.div>
     </div>
   );
 }
