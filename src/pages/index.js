6a67df119e74d80182cd9cdff444d73b806e37dd
chanmioh <chanmioh@gmail.com>
2023-04-13 14:02:45 -0400

    Added Google maps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,39 @@
-import Image from "next/image";
+import { useMemo, useState } from "react";
 import { Inter } from "next/font/google";
 import clinicsData from "../clinic-info.json";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  const clinics = clinicsData.map((c) => c.clinic_name);
-
-  return <>{clinics}</>;
+// Search clinics matching a given zipcode.
+function getClinicsInZipcode(zipcode) {
+  const filteredClinics = clinicsData.filter(
+    (clinic) => clinic.zip === zipcode
+  );
+  return filteredClinics;
 }
+
+export default function Home() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) return <>Loading...</>;
+
+  return <Map />;
+}
+
+const Map = () => {
+  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+  const [selected, setSelected] = useState(null);
+
+  return (
+    <>
+      <GoogleMap
+        mapContainerClassName="w-full h-screen"
+        zoom={10}
+        center={center}
+      ></GoogleMap>
+    </>
+  );
+};
