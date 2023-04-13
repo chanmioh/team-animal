2819dacc7fbd46ff3fa5cfcbb56597f0e9837f01
Sharon Yang <sharonyyang0@gmail.com>
2023-04-13 19:24:40 -0400

    Fixed address formatting issues

--- src/components/clinic.js
+++ src/components/clinic.js
@@ -1,11 +1,13 @@
 export default function Clinic({ clinic }) {
   const takesInsurance = "✔ Takes Insurance";
   const noInsurance = "❌ Doesn't Take Insurance";
+  const address = clinic.address1 + (clinic.adress2 ? " " + clinic.adress2 : "") + ", "
+
 
   return (
     <div className="border-b-2 space-y-1 px-10 py-6">
       <div className="font-medium">{clinic.clinic_name}</div>
-      <div>{clinic.address1} {clinic.adress2}, {clinic.city}, {clinic.state} {clinic.zip}</div>
+      <div>{address}{clinic.city}, {clinic.state} {clinic.zip}</div>
       <div>{clinic.phone}</div>
       <div>{clinic.accepts_insurance ? takesInsurance : noInsurance}</div>
     </div>
