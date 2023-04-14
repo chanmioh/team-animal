c9330a6a1810423b4a812096ebea900ec381c05e
Melody Liu <33408946+melocule@users.noreply.github.com>
2023-04-13 20:07:00 -0400

    Support unknown phone numbers

--- src/components/clinic.js
+++ src/components/clinic.js
@@ -2,13 +2,15 @@ export default function Clinic({ clinic }) {
   const takesInsurance = "✔ Takes Insurance";
   const noInsurance = "❌ Doesn't Take Insurance";
   const address = clinic.address1 + (clinic.adress2 ? " " + clinic.adress2 : "") + ", "
-  const phoneNum = "📞 (" + clinic.phone.substring(0, 3) + ")" + " " + clinic.phone.substring(3, 6) + "-" + clinic.phone.substring(6)
+  
+  const phoneNum = clinic.phone
+  let phoneNumDisplay = phoneNum ? "📞 (" + phoneNum.substring(0, 3) + ")" + " " + clinic.phone.substring(3, 6) + "-" + clinic.phone.substring(6) : "📞 Unknown"
   
   return (
     <div className="border-b-2 space-y-1 px-10 py-6">
       <div className="font-medium">{clinic.clinic_name}</div>
       <div>{address}{clinic.city}, {clinic.state} {clinic.zip}</div>
-      <div>{phoneNum}</div>
+      <div>{phoneNumDisplay}</div>
       <div>{clinic.accepts_insurance ? takesInsurance : noInsurance}</div>
     </div>
   );
