de106a3db17a007bbf632b1d99d667db0be82cb9
Sharon Yang <sharonyyang0@gmail.com>
2023-04-13 21:47:57 -0400

    Fixed formatting issue with address on details page

--- src/components/details.js
+++ src/components/details.js
@@ -23,14 +23,15 @@ export function Details(props) {
     const specialties = props.clinic.specialties || []
     const offerings = createAdditionalOfferingsArr(props.clinic.practiceHub, props.clinic.televet_services, props.clinic.emergency_services);
 
-    const address = props.clinic.address1 + ' ' + props.clinic.adress2 + ', ' + props.clinic.city + ', ' + props.clinic.state + ' '+ props.clinic.zip
+    const address = props.clinic.address1 + (props.clinic.adress2 ? " " + props.clinic.adress2 : "") + ", "
+    const fullAddress = address + props.clinic.city + ', ' + props.clinic.state + ' '+ props.clinic.zip
 
     const gMapLink = 'http://maps.google.com/?ll=' + props.clinic.latitude + ',' + props.clinic.longitude
 
 
     return (<div className="text-lg space-y-1 bg-white flex flex flex-col px-10 border-y-2 py-6">
         <div className="text-2xl font-medium">{props.clinic.clinic_name}</div>
-        <div><a href={gMapLink} target="_blank">{address}</a></div>
+        <div><a href={gMapLink} target="_blank">{fullAddress}</a></div>
         <div>{props.clinic.phone}</div>
         <div>{props.clinic.accepts_insurance ? takesInsurance : noInsurance}</div>
 
