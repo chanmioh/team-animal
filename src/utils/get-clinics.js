2765d89b3cc035ef1b884e2e42839755819ca395
chanmioh <chanmioh@gmail.com>
2023-04-14 02:09:28 -0400

    help

--- src/utils/get-clinics.js
+++ src/utils/get-clinics.js
@@ -1,9 +1,12 @@
 import clinicsData from "../../final_clinic_data.json";
+import haversine from "haversine-distance";
 
 // Get clinics matching given search params.
 export function getClinics(searchParams) {
-  let result = clinicsData;
+  // let result = getClinicsInRadius(searchParams.zipCode);
+  // console.log(result);
 
+  let result = clinicsData;
   // If there is a zipcode, filter for it in the clinics.
   if (searchParams.zipCode) {
     result = result.filter((clinic) => {
@@ -19,10 +22,9 @@ export function getClinics(searchParams) {
     );
   });
 
-  const categoriesAndSpecialties = [
-    ...searchParams.categories,
-    ...searchParams.specialties,
-  ];
+  const categoriesAndSpecialties = searchParams.categories.concat(
+    searchParams.specialties
+  );
 
   // For each category, find the matching clinics.
   for (let i = 0; i < categoriesAndSpecialties.length; i++) {
@@ -33,4 +35,30 @@ export function getClinics(searchParams) {
   console.log(result);
 
   return result;
+}
+
+export function getClinicsInRadius(zipcode) {
+  const radius = 1;
+  const matchingZipcodeClinic = clinicsData.find((c) => c.zip === zipcode);
+
+  if (matchingZipcodeClinic) {
+    const zipcodeCoords = {
+      lat: matchingZipcodeClinic.latitude,
+      lng: matchingZipcodeClinic.longitude,
+    };
+
+    console.log(zipcodeCoords);
+
+    const results = clinicsData.filter((c) => {
+      const dist =
+        haversine(zipcodeCoords, {
+          lat: parseFloat(c.latitude),
+          lng: parseFloat(c.longitude),
+        }) * 0.000621;
+      return dist <= radius;
+    });
+
+    return results;
+  }
+  return clinicsData;
 }
